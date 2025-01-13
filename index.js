a6bfd4fbc46c2c3777dfdd23fb9398838367924a
flavin-jr <fjcvj@cin.ufpe.br>
2025-01-13 16:28:32 -0300

    feat: add navigation to question pages

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 $(document).ready(function () {
-    $(".toggle").click(function (e) {
-        e.preventDefault();
-        console.log('on')
-        $(this).addClass("animation-active");;
+    $.ajax({
+        url: "/data.json",
+        type: "GET",
+        data: {},
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            const colors = {
+                'HTML': "#FFF1E9",
+                'CSS': "#E0FDEF",
+                'JavaScript': "#EBF0FF",
+                'Accessibility': "#F6E7FF",
+            }
+            $(".toggle").click(function (e) {
+                e.preventDefault();
+                console.log('on')
+                $(this).addClass("animation-active");
 
+            });
+
+            $(".option").click(function (e) {
+                e.preventDefault();
+
+                const subject = $(this).children(':eq(1)').text();
+                const data = response.quizzes.filter(item => item.title === subject);
+                const finalData = data[0];
+                const questions = finalData.questions;
+                $(".main").css("display", "none")
+                $(".theme-option").css("justify-content", "space-between")
+                $(".subject").css("display", "flex")
+                $(".subject").children(':eq(0)').attr({
+                    'src': finalData.icon,
+                    'alt': `Icone de ${subject}`
+                });
+                $(".subject").children(":eq(0)").css("background-color", colors[subject])
+
+                $(".subject").children(':eq(1)').text(finalData.title)
+                $(".html").css("display", "flex")
+
+
+            });
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", error); // Handle errors
+        },
+        complete: function () {
+            console.log("Request completed."); // Always executed
+        }
     });
 });
